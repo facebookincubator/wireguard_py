b55c15cfe7648a7b8481891d8d1c869021e46c53
David Angst <cmoi.ddsl@gmail.com>
2024-10-02 22:17:38 +0200

    Add last handshake time, rx bytes, and tx bytes to list_peers function

--- wireguard_py/wireguard_py.pyx
+++ wireguard_py/wireguard_py.pyx
@@ -312,6 +312,11 @@ def list_peers(device_name: bytes) -> List[Peer]:
                         break
 
                     allowedip = allowedip.next_allowedip
+            
+            # Add last_handshake_time, rx_bytes, and tx_bytes
+            peer_py.last_handshake_time = peer.last_handshake_time
+            peer_py.rx_bytes = peer.rx_bytes
+            peer_py.tx_bytes = peer.tx_bytes
 
             peers.append(peer_py)
 
