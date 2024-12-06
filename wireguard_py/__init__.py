b3dce5d3dfbe300ba5d66f2bd5a67a584ca5cef3
Thomas Polasek <thomaspolasek@meta.com>
2024-12-06 10:37:02 -0800

    Convert FBCODE to use the Ruff Formatter
    Summary:
    Converts the directory specified to use the Ruff formatter. This is the last big diff to convert all of Fbcode to Ruff.
    
    pomsky_fix_bugs
    
    drop-conflicts
    bypass-github-export-checks
    allow-large-files
    
    Reviewed By: amyreese
    
    Differential Revision: D66886610
    
    fbshipit-source-id: 8276a7f6164efec189ca0b87e535543ed5bc3615

--- wireguard_py/__init__.py
+++ wireguard_py/__init__.py
@@ -5,6 +5,7 @@ Copyright (c) Meta Platforms, Inc. and affiliates.
 This software may be used and distributed according to the terms of the
 MIT License.
 """
+
 __version__ = "trunk"
 
 from .wireguard_py import (
